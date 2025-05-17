0f40f7d0cee1bafea35a4f66ab1a63389263279e
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-05-17 15:46:51 +0100

    Added the navbar and ticket component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import TicketCard from './components/TicketCard'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <TicketCard/>
+    </div>
+  )
+}
+
+export default App
